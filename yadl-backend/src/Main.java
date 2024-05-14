290b4467e985eed62c733b5d7f79e6699c4e926c
Thoren Hirsch <t.hirsch@campus.tu-berlin.de>
2024-05-14 12:50:31 +0200

    14.05 1

--- yadl-backend/src/Main.java
+++ yadl-backend/src/Main.java
@@ -1,5 +1,31 @@
+import org.apache.flink.*;
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ExecutionEnvironment env = ExecutionEnvironment.createLocalEnvironment();
+
+        DataSet<String> data = env.readTextFile("file:///path/to/file");
+
+        data
+                .filter(new FilterFunction<String>() {
+                    public boolean filter(String value) {
+                        return value.startsWith("http://");
+                    }
+                })
+                .writeAsText("file:///path/to/result");
+
+        JobExecutionResult res = env.execute();
     }
+
 }
---
+++ yadl-backend/src/Test.java
@@ -0,0 +1,14 @@
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.util.JSONPObject;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Test {
+    public  void loadJson(String path){
+        //String content = new String(Files.readAllBytes(Paths.get(path)));
+
+        //JSONPObject jsonpObject = new JSONPObject(content);
+
+
+    }
+}
