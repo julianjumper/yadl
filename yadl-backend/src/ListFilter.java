ce94294803b8d5e4fadfb3b81328e700abf559b0
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-05-21 15:17:53 +0200

    move operation class to ListFilter

--- yadl-backend/src/ListFilter.java
+++ yadl-backend/src/ListFilter.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 
+enum Operation {
+    EQUALS, GREATER, LESS
+}
 public class ListFilter {
     public static <T> List<T> filter(List<T> list, Filter<T> filter) {
         List<T> result = new ArrayList<>();
