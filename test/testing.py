0d712bc15eb6528d6818d83b463f54fe7c865824
Benni <benjamin.ueber@campus.tu-berlin.de>
2024-06-04 14:36:53 +0200

    change default run command

--- test/testing.py
+++ test/testing.py
@@ -5,7 +5,7 @@ import subprocess
 import filecmp
 from pathlib import Path
 
-DEFAULT_RUN_COMMAND = "yadl-interpreter %s"
+DEFAULT_RUN_COMMAND = f"java -jar {os.getenv('YADL_JAR')} %s"
 
 
 def parse_comment(comment_line: str):
