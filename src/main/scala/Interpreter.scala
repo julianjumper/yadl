d5f476e159f9bb6663f08e56dd23b59c811ce3aa
Dennis Benedikt Schulze <dennis.schulze.1@campus.tu-berlin.de>
2024-06-13 19:42:26 +0200

    add conversions from ast node to data object and back
    + formating
    + calling to internal std

--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -1,15 +1,14 @@
 import scala.util.control.Breaks._
 
-import parser._ // TODO do not import everything.
-import parser.ArithmaticOps.{Add, Div, Expo, Mul, Sub}
-import parser.BooleanOps.{And, Or}
-import parser.CompareOps.{Eq, Greater, GreaterEq, Less, LessEq, NotEq}
+import parser._ // To lazy to import all types individually
 
-import stdlib.{BuiltinFunction, getStdlibDictionary}
+import ArithmaticOps.{Add, Div, Expo, Mul, Sub}
+import BooleanOps.{And, Or}
+import CompareOps.{Eq, Greater, GreaterEq, Less, LessEq, NotEq}
+
+val builtins = stdlib.stdlib
 
 type HashMap[K, V] = scala.collection.mutable.HashMap[K, V]
-
-// val builtinFunctions = stdlib.getStdlibDictionary()
 
 class Scope(
     parent: Scope = null,
@@ -69,6 +68,14 @@ def evalFunctionCall(
       printValue(res)
     print("\n")
     scope
+  } else if (builtins.contains(identifier)) {
+    val callArgsNew = callArgs.map { value =>
+      val Some(v) = evalValue(value, scope).result: @unchecked
+      interpreterdata.toDataObject(v)
+    }
+    val Some(func) = builtins.get(identifier): @unchecked
+    val result = func.function(callArgsNew)
+    scope.returnValue(interpreterdata.toAstNode(result))
   } else {
     scope.lookup(Identifier(identifier)) match {
       case Some(Function(args, body)) =>
@@ -146,11 +153,15 @@ def evalStatement(
 
 def evalWhileLoop(whileLoop: Branch, scope: Scope): Scope = {
   var currentScope = scope
-  while (evalValue(whileLoop.condition, currentScope).result.contains(Bool(true))) {
-    currentScope = whileLoop.body.foldLeft(currentScope)((accScope, statement) => {
-      if (accScope.hasResult) accScope // Early exit if result is set (like return statements)
-      else evalStatement(accScope, statement)
-    })
+  while (
+    evalValue(whileLoop.condition, currentScope).result.contains(Bool(true))
+  ) {
+    currentScope =
+      whileLoop.body.foldLeft(currentScope)((accScope, statement) => {
+        if (accScope.hasResult)
+          accScope // Early exit if result is set (like return statements)
+        else evalStatement(accScope, statement)
+      })
   }
   currentScope
 }
