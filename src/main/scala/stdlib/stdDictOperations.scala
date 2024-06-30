f740ae30efd3beec82a2b8f66ecd98be315f1727
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-06-30 15:03:14 +0200

    finalize

--- src/main/scala/stdlib/stdDictOperations.scala
+++ src/main/scala/stdlib/stdDictOperations.scala
@@ -1,62 +1,68 @@
 package stdlib
 
-import jsoniterator.JsonIterator
 import interpreterdata.*
+import jsoniterator.JsonIterator
 import scala.io.Source
+import java.nio.file.Paths
 import scala.collection.mutable
 
 
-private def loadFunction(params: Seq[DataObject]): DataObject = params match {
-  case Seq(StringObj(path), StringObj(format)) =>
-    format.toLowerCase match {
-      case "json" =>
-        try {
-          val source = Source.fromFile(path)
-          val iterator = new JsonIterator(source)
-          IteratorObj(
-            FunctionObj(Seq("d"), Seq(), None, _ => {
-              if (iterator.hasNext) {
-                iterator.next() match {
-                  case Right((key, value)) => convertToDataObject((key, value))
-                  case Left(error) => throw new RuntimeException(error)
-                }
-              } else {
-                throw new RuntimeException("No more elements")
-              }
-            }),
-            FunctionObj(Seq("d"), Seq(), None, _ => BooleanObj(iterator.hasNext)),
-            DictionaryObj(mutable.HashMap[DataObject, DataObject]())
-          )
-        } catch {
-          case e: Exception => throw new RuntimeException(s"Error loading JSON file: ${e.getMessage}")
-        }
-      case _ =>
-        throw new IllegalArgumentException(s"Unsupported format: $format")
+private def loadFunction(params: Seq[DataObject]): DataObject = {
+  if (params.length != 2 || !params(0).isInstanceOf[StringObj] || !params(1).isInstanceOf[StringObj]) {
+    throw IllegalArgumentException("Load function expects two string arguments: path and format")
+  }
+
+  val path = params(0).asInstanceOf[StringObj].value
+  val format = params(1).asInstanceOf[StringObj].value.toLowerCase
+
+  val currentDir = System.getProperty("user.dir")
+  val fullPath = Paths.get(currentDir, "test", "tests", path).toString
+  println(s"Loading file from: $fullPath")
+  val source = Source.fromFile(fullPath)
+
+  format match {
+    case "json" =>
+      val jsonIterator = new JsonIterator(source)
+      processJsonIterator(jsonIterator)
+    case _ => throw IllegalArgumentException(s"Unsupported format: $format")
+  }
+}
+
+private def processJsonIterator(iterator: JsonIterator): DataObject = {
+  val result = mutable.HashMap[String, DataObject]()
+
+  while (iterator.hasNext) {
+    iterator.next() match {
+      case Right((key, value)) =>
+        val valueObj = convertJsonValue(value)
+        result(key) = valueObj
+      case Left(error) =>
+        throw IllegalArgumentException(s"Error reading from iterator: $error")
     }
-  case _ =>
-    throw new IllegalArgumentException("load function requires two string arguments: path and format")
+  }
+
+  convertToProperStructure(result)
 }
 
-private def convertToDataObject(value: (String, Any)): DataObject = value match {
-  case (key, null) => DictionaryObj(mutable.HashMap(StringObj(key) -> NoneObj()))
-  case (key, b: Boolean) => DictionaryObj(mutable.HashMap(StringObj(key) -> BooleanObj(b)))
-  case (key, n: BigDecimal) => DictionaryObj(mutable.HashMap(StringObj(key) -> NumberObj(n.toDouble)))
-  case (key, s: String) => DictionaryObj(mutable.HashMap(StringObj(key) -> StringObj(s)))
-  case (key, i: JsonIterator) =>
-    DictionaryObj(mutable.HashMap(StringObj(key) -> IteratorObj(
-      FunctionObj(Seq("d"), Seq(), None, _ => {
-        if (i.hasNext) {
-          i.next() match {
-            case Right((k, v)) => convertToDataObject((k, v))
-            case Left(error) => throw new RuntimeException(error)
-          }
-        } else {
-          throw new RuntimeException("No more elements")
-        }
-      }),
-      FunctionObj(Seq("d"), Seq(), None, _ => BooleanObj(i.hasNext)),
-      DictionaryObj(mutable.HashMap[DataObject, DataObject]())
-    )))
-  case (_, v) => throw new IllegalArgumentException(s"Unsupported type: ${v.getClass.getSimpleName}")
+private def convertToProperStructure(map: mutable.HashMap[String, DataObject]): DataObject = {
+  if (map.keys.forall(_.matches("""\[\d+\]"""))) {
+    val sortedEntries = map.toSeq.sortBy { case (key, _) =>
+      key.substring(1, key.length - 1).toInt
+    }.map(_._2)
+    ListObj(mutable.ArrayBuffer(sortedEntries: _*))
+  } else {
+    DictionaryObj(map.map { case (k, v) => StringObj(k) -> v })
+  }
 }
 
+private def convertJsonValue(value: Any): DataObject = {
+  value match {
+    case null | None => NoneObj()
+    case b: Boolean => BooleanObj(b)
+    case n: BigDecimal => NumberObj(n.toDouble)
+    case s: String => StringObj(s)
+    case it: JsonIterator => processJsonIterator(it)
+    case other =>
+      throw IllegalArgumentException(s"Unsupported JSON value type: ${other.getClass}")
+  }
+}
