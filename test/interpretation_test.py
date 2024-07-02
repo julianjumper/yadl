be10ea3084da315ed6addc2c7f4b490e6ac4c4d7
Benni <benjamin.ueber@campus.tu-berlin.de>
2024-07-02 15:55:34 +0200

    oops maybe should test the working tests

--- test/interpretation_test.py
+++ test/interpretation_test.py
@@ -86,7 +86,7 @@ def run_test(test_cfg):
 
 
 configurations = []
-TEST_DIR = os.path.abspath("test/tests_todo")
+TEST_DIR = os.path.abspath("test/tests")
 for posix_path in Path(TEST_DIR).rglob("*.yadl"):
     full_path = os.path.join(os.path.dirname(TEST_DIR), posix_path)
     configurations.append(parse_yadl(str(full_path)))
