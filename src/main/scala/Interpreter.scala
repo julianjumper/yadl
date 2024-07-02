4aacd64ebf857633a43bd4b5f2dd538f94c06625
jmjumper <juli.springer@gmx.de>
2024-07-02 13:18:22 +0200

    Revert "Merge branch 'main' into '20-fix-for-nested-function-calls-separating-functions-from-variables'"
    This reverts commit 1f4162abadbe21448e568e56abee2e8371d14924, reversing
    changes made to b21389737fb2c5995cff4c7330a6f41ed2e02a85.

--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -463,8 +463,6 @@ def evalValue(
     case FormatString(value) => assert(false, "TODO: Format strings in eval implementation")
     case ArrayLiteral(elements) => 
       scope.returnValue(ArrayLiteral(elements))
-    case NoneValue() =>
-      scope.returnValue(NoneValue())
     case err =>
       assert(false, f"TODO: not implemented '$err'")
   }
@@ -539,10 +537,9 @@ def evalCompareOps(
       val lhs_num = extractNumber(value1)
       val rhs_num = extractNumber(value2)
       val result = op match {
-
-        case Less      => lhs_num < rhs_num
+        case Less      => lhs_num <  rhs_num
         case LessEq    => lhs_num <= rhs_num
-        case Greater   => lhs_num > rhs_num
+        case Greater   => lhs_num >  rhs_num
         case GreaterEq => lhs_num >= rhs_num
         case Eq        => lhs_num == rhs_num
         case NotEq     => lhs_num != rhs_num
@@ -610,9 +607,8 @@ def evalArithmeticOps(
 
 // Input: Number or Bool. Output: Double (true == 1, false == 0)
 def extractNumber(value: Value): Double = value match {
-  case Number(n)   => n
-  case Bool(b)     => if (b) 1.0 else 0.0
-  case NoneValue() => 0.0
+  case Number(n) => n
+  case Bool(b)   => if (b) 1.0 else 0.0
   case v => assert(false, s"Expected number or boolean in comparison. Got '$v'")
 }
 
