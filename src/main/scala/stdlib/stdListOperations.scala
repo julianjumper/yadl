19bee8efb944db529a177461b9f223f60d43e2ac
Thoren Hirsch <t.hirsch@campus.tu-berlin.de>
2024-07-02 12:27:55 +0200

    02.07

--- src/main/scala/stdlib/stdListOperations.scala
+++ src/main/scala/stdlib/stdListOperations.scala
@@ -101,3 +101,52 @@ private def flatMapBuiltIn(params: Seq[DataObject]): DataObject = {
 
   IteratorObj(next, hasnext, d)
 }
+
+private def flatMapBuiltIn(params: Seq[DataObject]): DataObject = {
+  if (params.length != 2 || !params(1).isInstanceOf[FunctionObj]) {
+    throw IllegalArgumentException()
+  }
+
+  val flatMapFn = params(1).asInstanceOf[FunctionObj]
+
+  // We can cheat and just don't use the the data attribute of the
+  // iterator, since it can't be accessed by the language in the
+  // first place.
+  val d = DictionaryObj(mutable.HashMap[DataObject, DataObject]())
+
+  val it = toIteratorObj(params(0)) //Function parameter + data
+  var buffer: ListObj = NONE
+  var hasBuffer = false
+
+  var arrSize = 0
+
+  val hasnext = FunctionObj(Seq("d"), Seq(), None, (d: Seq[DataObject]) => {
+    if (arrSize < buffer.size()) {
+      TRUE
+    }
+    while (it.hasNext.function(Seq(it.data)).asInstanceOf[BooleanObj].value) {
+      val n = it.next.function(Seq(it.data))
+      buffer = flatMapFn.function(Seq(n)).asInstanceOf[ListObj]
+      arrSize = 0
+      TRUE
+    }
+    FALSE
+  })
+
+  val next = FunctionObj(Seq("d"), Seq(), None, (d: Seq[DataObject]) => {
+    if (arrSize < buffer.size()) {
+      val tmp = buffer[arrSize]
+      arrSize += 1
+      tmp
+    } else if (hasnext.function(d).asInstanceOf[BooleanObj].value) {
+      val tmp = buffer[arrSize]
+      arrSize += 1
+      tmp
+    }
+    else {
+      throw IllegalArgumentException()
+    }
+  })
+
+  IteratorObj(next, hasnext, d)
+}
