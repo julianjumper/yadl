8706bae3034a88cde688466c31ff7dfde435e751
Dennis Benedikt Schulze <dennis.schulze.1@campus.tu-berlin.de>
2024-09-17 06:51:48 +0200

    refactor testing

--- test-scripts/scala/failing_test.py
+++ test-scripts/scala/failing_test.py
@@ -1,112 +1,21 @@
 import os
-import sys
-
 import pytest
-import subprocess
-import filecmp
 from pathlib import Path
 
-DEFAULT_RUN_COMMAND = f"java -jar {os.getenv('YADL_JAR')} '%s'"
-
-
-def parse_yadl(filepath):
-    test_cfg = {
-        "filepath": filepath,
-        "out": [],
-        "file-eq": [],
-        "remove": [],
-    }
-
-    with open(filepath, "r") as file:
-        lines = file.readlines()
-
-        if len(lines) == 0:
-            raise SyntaxError("Expected non empty yadl file")
-
-        for line in lines:
-            line = line.strip()
-            tokens = line.split(sep=" ")
-
-            # only comments are of interest
-            if not tokens[0] == "//":
-                continue
-
-            # %s is a placeholder for the filename
-            for i, token in enumerate(tokens):
-                if token == "%s":
-                    tokens[i] = filepath
-
-            # fill the config by checking for keywords
-            # define (alternative) command to run
-            if tokens[1] == "RUN:":
-                assert (
-                    "run" not in test_cfg
-                ), f'RUN command found multiple times in file "{filepath}"'
-
-                if tokens[2] == "DEFAULT":
-                    test_cfg["run"] = DEFAULT_RUN_COMMAND.replace(
-                        "%s", filepath)
-                else:
-                    test_cfg["run"] = " ".join(tokens[2:])
-            # check output
-            elif tokens[1] == "CHECK-OUT:":
-                if "out" not in test_cfg:
-                    test_cfg["out"] = []
-
-                test_cfg["out"].append(" ".join(tokens[2:]))
-            # check if two files are equal
-            elif tokens[1] == "CHECK-FILE-EQ:":
-                if "file-eq" not in test_cfg:
-                    test_cfg["file-eq"] = []
-
-                test_cfg["file-eq"].append(tokens[2:4])
-            # remove a file after the test
-            elif tokens[1] == "REMOVE:":
-                if "remove" not in test_cfg:
-                    test_cfg["remove"] = []
-
-                test_cfg["remove"].append(tokens[2])
-
-    return test_cfg
-
-
-def run_test(test_cfg):
-    print("trying to execute file:", test_cfg["filepath"])
-    result = subprocess.run(
-        test_cfg["run"], capture_output=True, shell=True, text=True)
-
-    # check output
-    output = result.stdout.strip().split("\n")
-    print("output of the program before exit:")
-    for line in output:
-        print(" ", line)
-    assert result.returncode == 0, f"subprocess failed: {result.stderr}"
-    assert output == test_cfg["out"]
-
-    # check file equalities
-    for files in test_cfg["file-eq"]:
-        assert filecmp.cmp(files[0], files[1], shallow=False)
-
-    # remove files
-    for file in test_cfg["remove"]:
-        os.remove(file)
-
+from helper import parse_yadl, run_test, to_dir
 
 configurations = []
 TEST_DIR = os.path.abspath("test/failing")
 for posix_path in Path(TEST_DIR).rglob("*.yadl"):
     full_path = os.path.join(os.path.dirname(TEST_DIR), posix_path)
-    configurations.append(parse_yadl(str(full_path)))
+    configurations.append((parse_yadl(str(full_path)), TEST_DIR))
+file_names = map(lambda t: to_dir(t[0], t[1]), configurations)
 
 
-def to_dir(config):
-    return str(Path(config["filepath"]).relative_to(TEST_DIR))
-
-
-@pytest.mark.parametrize("config", configurations, ids=map(to_dir, configurations))
+@pytest.mark.parametrize("config", configurations, ids=file_names)
 @pytest.mark.xfail(strict=True)
 def test_config(config):
-    run_test(config)
+    run_test(config[0])
 
 
 def pytest_collection_modifyitems(items):
